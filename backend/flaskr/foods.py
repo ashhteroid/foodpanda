e7c57694a0972b6349a8e2c53749813c71f16f6d
Ashwin R <ashwingopirams@gmail.com>
2020-08-17 07:38:54 -0700

    Added Second table

--- backend/flaskr/foods.py
+++ backend/flaskr/foods.py
@@ -6,31 +6,30 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
-# class Nutrient(db.Model):
-#     nutrient_id = db.Column(db.Integer, primary_key=True)
-#     nutrient_name = db.Column(db.String(80), unique=True, nullable=False)
-#     unit = db.Column(db.String(80), nullable=False)
-#     value_per_unit = db.Column(db.String(80))
-#     grams_per_100 = db.Column(db.Integer)
-#     food_id = db.Column(db.Integer, db.ForeignKey('food.ndbno'), nullable=False)
-#     food = db.relationship('Food', backref=db.backref('nutrient', lazy=True))
 
-#     def __repr__(self):
-#         return '<Nutrient %r>' % self.nutrient_name
 
 class Food(db.Model):
     ndbno = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     weight = db.Column(db.Integer, nullable=False)
     measure = db.Column(db.String(80), nullable=False)
-    nutrient_id = db.Column(db.Integer, primary_key=True)
-    nutrient_name = db.Column(db.String(80), nullable=False)
-    unit = db.Column(db.String(80), nullable=False)
-    value_per_unit = db.Column(db.String(80), nullable=False)
-    grams_per_100 = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
         return '<Food %r>' % self.name
+
+class Nutrient(db.Model):
+    nutrient_id = db.Column(db.Integer, primary_key=True)
+    nutrient_name = db.Column(db.String(80), unique=True, nullable=False)
+    unit = db.Column(db.String(80), nullable=False)
+    value_per_unit = db.Column(db.String(80))
+    grams_per_100 = db.Column(db.Integer)
+    food_id = db.Column(db.Integer, db.ForeignKey('food.ndbno'), nullable=False)
+    food = db.relationship('Food', backref=db.backref('nutrient', lazy=True))
+
+    def __repr__(self):
+        return '<Nutrient %r>' % self.nutrient_name
+
+        
 
 def update_database():
     with open('food_data.json') as f:
