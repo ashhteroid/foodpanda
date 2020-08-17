06d7bf2d3a218f21b2266d41a6a90d9789339ff0
Ashwin R <ashwingopirams@gmail.com>
2020-08-17 09:22:18 -0700

    removed venv from git tracking

--- backend/flaskr/foods.py
+++ backend/flaskr/foods.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import json
+from flask_restful import Resource, Api, reqparse
+
+
 
 app = Flask(__name__)
+api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
 
@@ -13,7 +17,7 @@ class Food(db.Model):
     measure = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return '<Food %r>' % self.name
+        return 'Food: ' + self.name
 
 class Nutrient(db.Model):
     nutrient_id = db.Column(db.Integer, primary_key=True)
@@ -25,7 +29,7 @@ class Nutrient(db.Model):
     food = db.relationship('Food', backref=db.backref('nutrient', lazy=True))
 
     def __repr__(self):
-        return '<Nutrient %r>' % self.nutrient_name
+        return ('Nutrient ' + self.nutrient_name + '| grams_per_100 ' + str(self.grams_per_100))
 
 def update_database():
     with open('food_data.json') as f:
@@ -34,8 +38,8 @@ def update_database():
     for food in foods:
         nutrients = food["nutrients"]
         for nutrient in nutrients:
-            current_nutrient = int(nutrient["nutrient_id"])
-            if current_nutrient in [203, 204, 205, 269]:
+            current_nutrient_id = int(nutrient["nutrient_id"])
+            if current_nutrient_id in [203, 204, 205, 269]:
                 if nutrient["gm"] == '--':
                     current_grams_per_100 = 0
                 else:
@@ -44,8 +48,9 @@ def update_database():
                                 name=food["name"], 
                                 weight=int(food["weight"]),
                                 measure=food["measure"])
-                nutrient_row = Nutrient(nutrient_id=current_nutrient,
-                                        nutrient_name=current_nutrient, 
+
+                nutrient_row = Nutrient(nutrient_id=current_nutrient_id,
+                                        nutrient_name=nutrient["nutrient"], 
                                         unit= nutrient["unit"], 
                                         value_per_unit=nutrient["value"],
                                         grams_per_100=current_grams_per_100,
@@ -54,15 +59,41 @@ def update_database():
         db.session.add(food_row)
     db.session.commit()
 
-db.drop_all()
-db.create_all()
-update_database()
-# print (Food.query.all())
+class GetFoods(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('protein', type=list)
+        parser.add_argument('carbohydrate', type=list)
+        parser.add_argument('sugar', type=list)
+        parser.add_argument('269' ,type=str )
+        print(dir(parser.add_argument))
+        d = parser.parse_args()
+        print (d)
 
+        result = list(db.session.query(Nutrient).join(Food, Food.ndbno==Nutrient.food_id)\
+                 .values(Nutrient.nutrient_id, Nutrient.nutrient_name, Food.name))
 
-@app.route('/api/foods')
-def get_current_time():
-    return {'time': time.time()}
+        a = Nutrient.query.filter_by(nutrient_id='203',unit="g").first()
+        #a = Nutrient.query.filter(Nutrient.grams_per_100 < int(d['203'])).filter(Nutrient.nutrient_id=="203").first()
+        print (result[0])
+        
+        return d
 
+api.add_resource(GetFoods, '/api/foods')
+
+# @app.route('/api/foods')
+# def get_current_time():
+#     return 'time'
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    # print (Food.query.all())
+
+    db.drop_all()
+    db.create_all()
+    update_database()
+    app.debug = True
+    app.run()
 
 
